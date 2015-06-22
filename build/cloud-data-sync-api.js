b20e5d7dc3107ecebaff186d5ac9e2c3ff4b9ef4
Sergey Konstantinov <twirl-team@yandex.ru>
2015-06-22 15:00:47 +0300

    Пересборка

--- build/cloud-data-sync-api.js
+++ build/cloud-data-sync-api.js
@@ -1891,9 +1891,12 @@ Client.prototype.initialize = function (options) {
  * аутентифицирован, false в противном случае.
  * @static
  */
-Client.prototype.isInitiaized = function () {
+Client.prototype.isInitialized = function () {
     return this._initialized;
 };
+
+// #12 — оставлено для обратной совместимости.
+Client.prototype.isInitiaized = Client.prototype.isInitialized
 
 /**
  * @name cloud.client.getToken
@@ -2467,56 +2470,6 @@ ns.modules.define('component.util', function (provide) {
 
     provide(util);
 });
-ns.modules.define('cloud.dataSyncApi.Conflict', [
-    'component.util'
-], function (provide, util) {
-    /**
-     * @class Описание конфликта, который может произойти
-     * при попытке применить некорректную транзакцию.
-     * @name cloud.dataSyncApi.Conflict
-     * @noconstructor
-     */
-    var Conflict = function (properties) {
-            this._type = properties.type;
-            this._fieldChangeConflicts = properties.field_change_conflicts;
-        };
-
-    util.defineClass(Conflict, /** @lends cloud.dataSyncApi.Conflict.prototype */ {
-        /**
-         * <p>Возвращает тип конфликта. Допустимые значения:</p>
-         * <ul>
-         *     <li>record_already_exists — возникает при попытке
-         * создать запись с идентификатором, который уже есть в указанной
-         * коллекции;</li>
-         *     <li>delete_non_existent_record — возникает при попытке
-         * удалить несуществующую запись;</li>
-         *     <li>update_non_existent_record — возникает при попытке
-         * изменить несуществующую запись;</li>
-         *     <li>both_modified — возникает в ситуации, когда
-         * запись была изменена на удалённом сервере;</li>
-         *     <li>invalid_field_change — возникает при попытке выполнения
-         * некорректной операции над отдельным полем записи.</li>
-         * </ul>
-         * @returns {String} Тип конфликта.
-         */
-        getType: function () {
-            return this._type;
-        },
-
-        /**
-         * Для конфликтов типа invalid_field_change возвращает детализацию
-         * конфликта: какая из операций над полями приводит к ошибке.
-         * @returns {Object[]} Конфликты при изменении полей записи
-         * в виде массива json-объектов с полями index (индекс операции)
-         * и type (тип конфликта).
-         */
-        getFieldChangeConflicts: function () {
-            return this._fieldChangeConflicts;
-        }
-    });
-
-    provide(Conflict);
-});
 ns.modules.define('component.xhr', [
     'global',
     'vow',
@@ -2622,6 +2575,56 @@ ns.modules.define('component.xhr', [
             }
         });
     });
+});
+ns.modules.define('cloud.dataSyncApi.Conflict', [
+    'component.util'
+], function (provide, util) {
+    /**
+     * @class Описание конфликта, который может произойти
+     * при попытке применить некорректную транзакцию.
+     * @name cloud.dataSyncApi.Conflict
+     * @noconstructor
+     */
+    var Conflict = function (properties) {
+            this._type = properties.type;
+            this._fieldChangeConflicts = properties.field_change_conflicts;
+        };
+
+    util.defineClass(Conflict, /** @lends cloud.dataSyncApi.Conflict.prototype */ {
+        /**
+         * <p>Возвращает тип конфликта. Допустимые значения:</p>
+         * <ul>
+         *     <li>record_already_exists — возникает при попытке
+         * создать запись с идентификатором, который уже есть в указанной
+         * коллекции;</li>
+         *     <li>delete_non_existent_record — возникает при попытке
+         * удалить несуществующую запись;</li>
+         *     <li>update_non_existent_record — возникает при попытке
+         * изменить несуществующую запись;</li>
+         *     <li>both_modified — возникает в ситуации, когда
+         * запись была изменена на удалённом сервере;</li>
+         *     <li>invalid_field_change — возникает при попытке выполнения
+         * некорректной операции над отдельным полем записи.</li>
+         * </ul>
+         * @returns {String} Тип конфликта.
+         */
+        getType: function () {
+            return this._type;
+        },
+
+        /**
+         * Для конфликтов типа invalid_field_change возвращает детализацию
+         * конфликта: какая из операций над полями приводит к ошибке.
+         * @returns {Object[]} Конфликты при изменении полей записи
+         * в виде массива json-объектов с полями index (индекс операции)
+         * и type (тип конфликта).
+         */
+        getFieldChangeConflicts: function () {
+            return this._fieldChangeConflicts;
+        }
+    });
+
+    provide(Conflict);
 });
 ns.modules.define('cloud.dataSyncApi.Database', [
     'cloud.dataSyncApi.config',
@@ -3573,116 +3576,6 @@ ns.modules.define('cloud.dataSyncApi.FieldOperation', [
 
     provide(FieldOperation);
 });
-ns.modules.define('cloud.dataSyncApi.Operation', [
-    'cloud.dataSyncApi.FieldOperation',
-    'cloud.dataSyncApi.Record',
-    'component.util'
-], function (provide, FieldOperation, Record, util) {
-    /**
-     * @class Операция над БД.
-     * @name cloud.dataSyncApi.Operation
-     * @param {Object} properties Параметры операции.
-     * @param {String} properties.type <p>Тип операции. Возможны следующие значения:</p>
-     * <ul>
-     *     <li>insert — добавление записи;</li>
-     *     <li>delete — удаление записи;</li>
-     *     <li>set — перезадание записи;</li>
-     *     <li>update — изменение отдельных полей записи.</li>
-     * </ul>
-     * @param {String} properties.collection_id Идентфикатор коллекции, над которой
-     * выполняется операция.
-     * @param {String} properties.record_id Идентификатор записи, над которой выполняется
-     * операция.
-     * @param {cloud.dataSyncApi.FieldOperation[]|Object[]} [properties.field_operations = []] Для операций
-     * insert, set и update задаёт изменения значений полей. Может задаваться массивом
-     * экземпляров класса {@link cloud.dataSyncApi.FieldOperation} либо json-объектов, приводимых к нему.
-     */
-    var Operation = function (properties) {
-            this._type = properties.type;
-            this._collectionId = properties.collection_id;
-            this._recordId = properties.record_id;
-            if (['insert', 'update', 'set'].indexOf(this._type) != -1) {
-                this._fieldOperations = (properties.field_operations || []).map(function (change) {
-                    if (change instanceof FieldOperation) {
-                        return change;
-                    } else {
-                        return new FieldOperation(change);
-                    }
-                });
-            } else {
-                this._fieldOperations = null;
-            }
-        };
-
-    Operation.json = {
-        serialize: function (operation) {
-            var type = operation.getType(),
-                res = {
-                    record_id: operation.getRecordId(),
-                    collection_id: operation.getCollectionId(),
-                    change_type: type
-                };
-
-            if (['insert', 'update', 'set'].indexOf(type) != -1) {
-                res.changes = operation.getFieldOperations().map(function (change) {
-                    return FieldOperation.json.serialize(change);
-                });
-            }
-
-            return res;
-        },
-
-        deserialize: function (serialized) {
-            var type = serialized.change_type,
-                properties = {
-                    type: type,
-                    record_id: serialized.record_id,
-                    collection_id: serialized.collection_id
-                };
-
-            if (['insert', 'update', 'set'].indexOf(this._type) != -1) {
-                properties.field_changes = serialized.changes.map(function (change) {
-                    return FieldOperation.json.deserialize(change)
-                });
-            }
-
-            return new Operation(properties);
-        }
-    };
-
-    util.defineClass(Operation, /** @lends cloud.dataSyncApi.Operation.prototype */ {
-        /**
-         * @returns {String} Тип операции.
-         */
-        getType: function () {
-            return this._type;
-        },
-
-        /**
-         * @returns {String} Возвращает идентификатор записи.
-         */
-        getRecordId: function () {
-            return this._recordId;
-        },
-
-        /**
-         * @returns {String} Возвращает идентификатор коллекции.
-         */
-        getCollectionId: function () {
-            return this._collectionId;
-        },
-
-        /**
-         * @returns {cloud.dataSyncApi.FieldOperation[]} Возвращает список
-         * операций над полями записи.
-         */
-        getFieldOperations: function () {
-            return this._fieldOperations ? this._fieldOperations.slice() : null;
-        }
-    });
-
-    provide(Operation);
-});
 ns.modules.define('cloud.dataSyncApi.Record', [
     'cloud.Error',
     'component.util',
@@ -3895,6 +3788,116 @@ ns.modules.define('cloud.dataSyncApi.Record', [
     provide(Record);
 });
 
+ns.modules.define('cloud.dataSyncApi.Operation', [
+    'cloud.dataSyncApi.FieldOperation',
+    'cloud.dataSyncApi.Record',
+    'component.util'
+], function (provide, FieldOperation, Record, util) {
+    /**
+     * @class Операция над БД.
+     * @name cloud.dataSyncApi.Operation
+     * @param {Object} properties Параметры операции.
+     * @param {String} properties.type <p>Тип операции. Возможны следующие значения:</p>
+     * <ul>
+     *     <li>insert — добавление записи;</li>
+     *     <li>delete — удаление записи;</li>
+     *     <li>set — перезадание записи;</li>
+     *     <li>update — изменение отдельных полей записи.</li>
+     * </ul>
+     * @param {String} properties.collection_id Идентфикатор коллекции, над которой
+     * выполняется операция.
+     * @param {String} properties.record_id Идентификатор записи, над которой выполняется
+     * операция.
+     * @param {cloud.dataSyncApi.FieldOperation[]|Object[]} [properties.field_operations = []] Для операций
+     * insert, set и update задаёт изменения значений полей. Может задаваться массивом
+     * экземпляров класса {@link cloud.dataSyncApi.FieldOperation} либо json-объектов, приводимых к нему.
+     */
+    var Operation = function (properties) {
+            this._type = properties.type;
+            this._collectionId = properties.collection_id;
+            this._recordId = properties.record_id;
+            if (['insert', 'update', 'set'].indexOf(this._type) != -1) {
+                this._fieldOperations = (properties.field_operations || []).map(function (change) {
+                    if (change instanceof FieldOperation) {
+                        return change;
+                    } else {
+                        return new FieldOperation(change);
+                    }
+                });
+            } else {
+                this._fieldOperations = null;
+            }
+        };
+
+    Operation.json = {
+        serialize: function (operation) {
+            var type = operation.getType(),
+                res = {
+                    record_id: operation.getRecordId(),
+                    collection_id: operation.getCollectionId(),
+                    change_type: type
+                };
+
+            if (['insert', 'update', 'set'].indexOf(type) != -1) {
+                res.changes = operation.getFieldOperations().map(function (change) {
+                    return FieldOperation.json.serialize(change);
+                });
+            }
+
+            return res;
+        },
+
+        deserialize: function (serialized) {
+            var type = serialized.change_type,
+                properties = {
+                    type: type,
+                    record_id: serialized.record_id,
+                    collection_id: serialized.collection_id
+                };
+
+            if (['insert', 'update', 'set'].indexOf(this._type) != -1) {
+                properties.field_changes = serialized.changes.map(function (change) {
+                    return FieldOperation.json.deserialize(change)
+                });
+            }
+
+            return new Operation(properties);
+        }
+    };
+
+    util.defineClass(Operation, /** @lends cloud.dataSyncApi.Operation.prototype */ {
+        /**
+         * @returns {String} Тип операции.
+         */
+        getType: function () {
+            return this._type;
+        },
+
+        /**
+         * @returns {String} Возвращает идентификатор записи.
+         */
+        getRecordId: function () {
+            return this._recordId;
+        },
+
+        /**
+         * @returns {String} Возвращает идентификатор коллекции.
+         */
+        getCollectionId: function () {
+            return this._collectionId;
+        },
+
+        /**
+         * @returns {cloud.dataSyncApi.FieldOperation[]} Возвращает список
+         * операций над полями записи.
+         */
+        getFieldOperations: function () {
+            return this._fieldOperations ? this._fieldOperations.slice() : null;
+        }
+    });
+
+    provide(Operation);
+});
 ns.modules.define('cloud.dataSyncApi.Transaction', [
     'cloud.dataSyncApi.Record',
     'cloud.dataSyncApi.Operation',
